79095b498c982b1be49a901ae9f70993eadb1b3e
buiduycuong97 <duycuongbui97@gmail.com>
2023-01-18 01:38:31 +0700

    fix start

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -95,13 +95,13 @@ access : private - login
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     //files khi upload xong sẽ nằm trong thư mục "uploads" này - các bạn có thể tự định nghĩa thư mục này
-    cb(null, "/public/images");
+    cb(null, "public/images");
   },
   filename: function (req, file, cb) {
     // tạo tên file = thời gian hiện tại nối với số ngẫu nhiên => tên file chắc chắn không bị trùng
-    // const filename = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    // cb(null, filename + "-" + file.originalname);
-    cb(null, file.originalname);
+    const filename = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, filename + "-" + file.originalname);
+    // cb(null, file.originalname);
   },
 });
 //Khởi tạo middleware với cấu hình trên, lưu trên local của server khi dùng multer
